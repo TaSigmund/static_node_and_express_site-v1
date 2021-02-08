162855188f91dbccffdd67b64b50abe4e7477c77
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-02-08 13:31:21 +0100

    error handlers

--- app.js
+++ app.js
@@ -2,7 +2,6 @@
 const express = require('express');
 const app = express();
 const pug = require('pug');
-
 const { projects } = require('./data/data.json');
 
 /* view engine */
@@ -24,6 +23,35 @@ app.use('/static', express.static('public'));
     const project = projects.find( ({ id }) => id === +projectId );
     res.render("project", {project})
 });
+
+/*** 
+ 404 handler 
+ ***/
+
+app.use((req, res, next)=>{
+    const err = new Error();
+    err.status = 404;
+    err.message = 'The page you are looking for does not exist.'
+    console.log(err.message, err.status);
+    next(err);
+})
+
+/*** 
+ global error handler 
+ ***/
+app.use((err, req, res, next) => {
+    if (!err.status) {err.status = 500};
+    if (!err.message) {err.message = 'A server error has occured'};
+    console.log(err.status, err.message);
+    res.status(err.status);
+    if (err.status === 404) {
+        res.render('notfound', { err })
+    }
+    else {
+        res.render('error', { err })
+    }
+}
+)
 
 /* server */
 app.listen(3000)
