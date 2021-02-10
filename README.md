0e23d9bb927686253d643d2ebdd726c6421fbf57
TaSigmund <64551452+TaSigmund@users.noreply.github.com>
2021-02-10 11:09:38 +0100

    style changes and comments

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
 CSS changes:
+I changed the css at the end of styles.css
 I changed the color of the sidebar so it matches the content views.
 I therefor had to change the h5 to black.
 I changed the font-family of the headings so it matches the rest of the webpage ('Lato', 'Helvetica Neue', Helvetica, Arial, sans-serif;)
+I changed the box-shadow of the sidebar thumbnail so it matches the project thumbnails.
+I also changed the background color of the links on the about page.
+I added a thicker seperation line on the index page.
+
+
 
 
--- app.js
+++ app.js
@@ -14,21 +14,26 @@ app.use('/static', express.static('public'));
  ROUTES
  ***/
 
+ /* index route */
  app.get('/', (req, res)=>{res.render("index", {projects})});
 
+ /* about route */
  app.get('/about', (req, res)=>{res.render("about")});
 
+
+/* project routes */
  app.get('/projects/:id', (req, res, next)=>{
-    const projectId = req.params.id;
-    const project = projects.find( ({ id }) => id === +projectId );
+    const projectId = req.params.id; //checks the url
+    const project = projects.find( ({ id }) => id === +projectId ); //finds the correct project
     if (!project) {next()} //makes sure there actually is a project to display before rendering
-    res.render("project", {project})
+    res.render("project", {project}) //renders the view
 });
 
 /*** 
  404 handler 
  ***/
 
+ /* creates an error for undefined routes and hands it to the global error handler */
 app.use((req, res, next)=>{
     const err = new Error();
     err.status = 404;
@@ -41,10 +46,12 @@ app.use((req, res, next)=>{
  global error handler 
  ***/
 app.use((err, req, res, next) => {
-    if (!err.status) {err.status = 500};
-    if (!err.message) {err.message = 'A server error has occured'};
+    if (!err.status) {err.status = 500}; //sets a status if necessary
+    if (!err.message) {err.message = 'A server error has occured'}; //sets an error message if necessary
     console.log(err.status, err.message);
-    res.status(err.status);
+    res.status(err.status); //returns the status
+    
+    /* the following lines render the appropriate views */
     if (err.status === 404) {
         res.render('page-not-found', { err })
     }
